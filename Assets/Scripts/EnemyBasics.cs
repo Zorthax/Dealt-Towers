6b625b18ba103231af86120943dc17ce56096641
Adam Schmidt <ad.schmidt7@gmail.com>
2016-11-30 20:54:27 +1030

    Initial commit

---
+++ Assets/Scripts/EnemyBasics.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBasics : MonoBehaviour {
+
+    public float speed;
+    int index = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (index < LevelPath.path.Length) FollowPath();
+	}
+
+    void FollowPath()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, LevelPath.path[index], speed);
+        if (transform.position == LevelPath.path[index]) index++;
+    }
+}
---
+++ Assets/Scripts/Map/LevelPath.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelPath : MonoBehaviour {
+
+    public static Vector3[] path;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Waypoint[] waypoints = Object.FindObjectsOfType<Waypoint>();
+        path = new Vector3[waypoints.Length];
+        foreach (Waypoint w in waypoints)
+        {
+            path[w.orderNumber] = w.transform.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
